15af7c73455b9660117cf71826af297f7666bfb9
minnyww <minrrverse@gmail.com>
2018-07-21 18:58:21 +0700

    [Update] Edit Style and install vector-icon

--- webpage/login/containner/Signin.js
+++ webpage/login/containner/Signin.js
@@ -23,57 +23,67 @@ export default class Signin extends React.Component {
 
 
   state = {
-    email : '',
-    password : '',
+    email: '',
+    password: '',
   }
-  
-   Login = () => {
-      console.log('email :'+this.state.email,'password :'+this.state.password);
+
+  Login = () => {
+    console.log('email :' + this.state.email, 'password :' + this.state.password);
 
   }
 
-  setField = (field,value) => {
-     this.setState({ [field]: value });
+  setField = (field, value) => {
+    this.setState({ [field]: value });
   }
-  
-  render(){
-        return <Container>
-        <Header span hasSegment style={styles.Header}>
-          <Left>
-            <Button transparent>
-              <Icon style={styles.icon} name="arrow-back" />
-            </Button>
-          </Left>
-          <Body>
-            <Segment style={styles.Header}>
-              <Button first style={styles.buttonHeader}>
-                <Text>Sign in</Text>
-              </Button>
-              <Button last active>
-                <Text>Sign Up</Text>
-              </Button>
-            </Segment>
-          </Body>
-          <Right />
-        </Header>
-        <Content>
-          <Text style={{ fontSize: 72, textAlign: "center" }}>Logo</Text>
-          <Form style={styles.Form}>
-            <Item floatingLabel first>
-              <Label>E-mail</Label>
-              <Input onChangeText={text => this.setField("email", text)} />
-            </Item>
-            <Item floatingLabel last>
-              <Label>Password</Label>
-              <Input onChangeText={text => this.setField("password", text)} />
-            </Item>
-          </Form>
-          <Text style={styles.Text}>FORGOT PASSWORD ?</Text>
-          <Button onPress={() => navigation.navigate("Welcome")} block dark style={styles.buttonSignin}>
-            <Text style={styles.textSignin}>Sign In</Text>
+
+  render() {
+    return
+    <Container style={styles.parent}>
+      <Header span hasSegment style={styles.Header}>
+        <Left>
+          <Button transparent>
+            <Icon style={styles.icon} name="chevron-left" />
           </Button>
-        </Content>
-      </Container>;
+        </Left>
+        <Body>
+          <Segment style={styles.Header}>
+            <Button first style={styles.buttonHeader}>
+              <Text>Sign in</Text>
+            </Button>
+            <Button last active>
+              <Text>Sign Up</Text>
+            </Button>
+          </Segment>
+        </Body>
+        <Right />
+      </Header>
+      <Content style={styles.content}>
+        <Text style={{ fontSize: 72, textAlign: "center" }}>Logo</Text>
+        <Form style={styles.Form}>
+          <Item floatingLabel first>
+            <Label>E-mail</Label>
+            <Input onChangeText={text => this.setField("email", text)} />
+          </Item>
+          <Item floatingLabel >
+            <Label>Password</Label>
+            <Input
+              secureTextEntry={true}
+              onChangeText={text => this.setField("password", text)}
+            />
+          </Item>
+        </Form>
+        <Text style={styles.Text}>FORGOT PASSWORD ?</Text>
+        <Button
+          onPress={() => navigation.navigate("Welcome")}
+          block
+          dark
+          style={styles.buttonSignin}
+        >
+          <Text style={styles.textSignin}>Sign In</Text>
+        </Button>
+      </Content>
+    </Container>
+      ;
   }
 
 }
@@ -82,24 +92,24 @@ export default class Signin extends React.Component {
 
 
 
- 
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     // isReady: false,
-  //     email : '',
-  //     password : '',
-  //   };
-  // }
 
-  // async componentWillMount() {
-    // await Expo.Font.loadAsync({
-    //   Roboto: require("native-base/Fonts/Roboto.ttf"),
-    //   Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-    //   Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
-    // });
-    // this.setState({ isReady: true });
-  // }
+// constructor() {
+//   super();
+//   this.state = {
+//     // isReady: false,
+//     email : '',
+//     password : '',
+//   };
+// }
+
+// async componentWillMount() {
+// await Expo.Font.loadAsync({
+//   Roboto: require("native-base/Fonts/Roboto.ttf"),
+//   Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+//   Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+// });
+// this.setState({ isReady: true });
+// }
 //   render() {
 //     // if (!this.state.isReady) {
 //     //   return <Expo.AppLoading />;
@@ -145,6 +155,12 @@ export default class Signin extends React.Component {
 //   }
 // }
 const styles = StyleSheet.create({
+  parent: {
+    flex: 1
+  },
+  content: {
+    flex: 1
+  },
   Text: {
     color: "grey",
     textAlign: "center",
@@ -161,13 +177,13 @@ const styles = StyleSheet.create({
     textAlign: "center",
     width: "80%",
     marginLeft: "10%",
-    marginRight: "10%",
-    marginTop : '20%'
+    marginRight: "15%",
+    marginTop: "20%"
   },
   buttonHeader: {
     color: "white",
     backgroundColor: "black",
-    borderRadius : 10
+    borderRadius: 10
   },
   icon: {
     color: "black"
@@ -178,8 +194,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginLeft: "10%",
     marginRight: "10%",
-    marginTop: 20 , 
-    borderRadius : 10
+    marginTop: 20,
+    borderRadius: 10
   },
   textSignin: {
     fontSize: 24,
